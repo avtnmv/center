8cbad2426d473bacc396c92aad7e38ef6dc011bc
Yaroslav Avtonomov <avtnmv@icloud.com>
2025-02-18 21:22:31 +0200

    update

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,3 +1,18 @@
+// Функция для установки disabled-класса на кнопки навигации
+function updateNavigationButtons(swiper, prevBtn, nextBtn) {
+    if (swiper.isBeginning) {
+        document.querySelector(prevBtn).classList.add('disabled');
+    } else {
+        document.querySelector(prevBtn).classList.remove('disabled');
+    }
+
+    if (swiper.isEnd) {
+        document.querySelector(nextBtn).classList.add('disabled');
+    } else {
+        document.querySelector(nextBtn).classList.remove('disabled');
+    }
+}
+
 // Инициализация Swiper для видео
 let swiperVideos = new Swiper('.mySwiperVideos', {
     slidesPerView: 6,
@@ -11,24 +26,32 @@ let swiperVideos = new Swiper('.mySwiperVideos', {
         el: '.custom-swiper-pagination-video',
         type: 'progressbar',
     },
-    allowTouchMove: true, // Включаем свайпы для всех устройств
+    allowTouchMove: true,
     breakpoints: {
         320: {
             slidesPerView: 2,
             spaceBetween: 10,
-            allowTouchMove: true, // Разрешаем свайп на мобильных устройствах
+            allowTouchMove: true,
         },
         576: {
             slidesPerView: 3,
             spaceBetween: 15,
-            allowTouchMove: true, // Разрешаем свайп на мобильных устройствах
+            allowTouchMove: true,
         },
         950: {
             slidesPerView: 6,
             spaceBetween: 10,
-            allowTouchMove: false, // Отключаем свайпы на больших экранах
+            allowTouchMove: false,
         }
     },
+    on: {
+        init: function () {
+            updateNavigationButtons(this, '.custom-prev-video', '.custom-next-video');
+        },
+        slideChange: function () {
+            updateNavigationButtons(this, '.custom-prev-video', '.custom-next-video');
+        }
+    }
 });
 
 // Инициализация Swiper для изображений
@@ -44,42 +67,30 @@ let swiperImages = new Swiper('.mySwiperImages', {
         el: '.custom-swiper-pagination-image',
         type: 'progressbar',
     },
-    allowTouchMove: true, // Включаем свайпы для всех устройств
+    allowTouchMove: true,
     breakpoints: {
         320: {
             slidesPerView: 2,
             spaceBetween: 10,
-            allowTouchMove: true, // Разрешаем свайп на мобильных устройствах
+            allowTouchMove: true,
         },
         576: {
             slidesPerView: 3,
             spaceBetween: 15,
-            allowTouchMove: true, // Разрешаем свайп на мобильных устройствах
+            allowTouchMove: true,
         },
         950: {
             slidesPerView: 6,
             spaceBetween: 10,
-            allowTouchMove: false, // Отключаем свайпы на больших экранах
+            allowTouchMove: false,
         }
     },
     on: {
         init: function () {
-            if (this.isBeginning) {
-                document.querySelector('.custom-prev-image').classList.add('disabled');
-            }
-            if (this.isEnd) {
-                document.querySelector('.custom-next-image').classList.add('disabled');
-            }
+            updateNavigationButtons(this, '.custom-prev-image', '.custom-next-image');
         },
-        reachBeginning: function () {
-            document.querySelector('.custom-prev-image').classList.add('disabled');
-        },
-        reachEnd: function () {
-            document.querySelector('.custom-next-image').classList.add('disabled');
-        },
-        fromEdge: function () {
-            document.querySelector('.custom-prev-image').classList.remove('disabled');
-            document.querySelector('.custom-next-image').classList.remove('disabled');
+        slideChange: function () {
+            updateNavigationButtons(this, '.custom-prev-image', '.custom-next-image');
         }
     }
 });
